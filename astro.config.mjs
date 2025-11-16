d185d46d45d244f28a0fc6c27a6a991b398f2f6e
antolikjan <antolikjan@gmail.com>
2025-11-16 11:08:32 +0100

    github deploy bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://antolikjan.github.io/",
-  base : "/sonomyo"
+  base : "/sonomyo",
   integrations: [
     tailwind(),
     icon(),
